474d58d2ec20a2c0b0fcd18f8625672a6ba3d79a
Ayoola Ogunbase <ogunbaseayoola@gmail.com>
2023-03-26 21:56:38 +0100

    Added another project

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,9 +4,11 @@ export const projectArr = [
     name: "bueno food",
     utilities:
       "React, Tailwind css, Swiper js, Framer motion, Google map api, Adobe Xd",
-    video: 'https://res.cloudinary.com/dgdoymhtj/video/upload/v1673183868/my-portfolio/bueno_bxcota.mp4',
+    video:
+      "https://res.cloudinary.com/dgdoymhtj/video/upload/v1673183868/my-portfolio/bueno_bxcota.mp4",
     img: "https://res.cloudinary.com/dgdoymhtj/image/upload/v1673182211/my-portfolio/bueno_ioehyq.png",
-    link1: "https://637af300e44885454b5a08d6--stately-piroshki-7dfcef.netlify.app/",
+    link1:
+      "https://637af300e44885454b5a08d6--stately-piroshki-7dfcef.netlify.app/",
     link2: "https://github.com/akinlope/Bueno",
   },
   {
@@ -23,7 +25,8 @@ export const projectArr = [
     id: 3,
     name: "shooot",
     utilities: "React, Css, Framer motion, Swiper js, Adobe Xd",
-    video: "https://res.cloudinary.com/dgdoymhtj/video/upload/v1673183957/my-portfolio/shoot_haw2br.mp4",
+    video:
+      "https://res.cloudinary.com/dgdoymhtj/video/upload/v1673183957/my-portfolio/shoot_haw2br.mp4",
     img: "https://res.cloudinary.com/dgdoymhtj/image/upload/v1673182217/my-portfolio/shooot_pyunc9.png",
     link1: "https://tomiwacode-shooot.netlify.app/",
     link2: "https://github.com/tomiwa-code/shooot",
@@ -32,9 +35,21 @@ export const projectArr = [
     id: 4,
     name: "my portfolio",
     utilities: "React, Tailwind Css, Framer motion, Css, Adobe Xd",
-    video: "",
-    img: "https://res.cloudinary.com/dgdoymhtj/image/upload/v1673182211/my-portfolio/myportfolio_dziw8p.png",
+    video:
+      "https://res.cloudinary.com/dgdoymhtj/video/upload/v1679863985/my-portfolio/my-portfolio_unrltm.mp4",
+    img: "https://res.cloudinary.com/dgdoymhtj/image/upload/v1679864150/my-portfolio/my-portfolio_xtjnnj.png",
     link1: "https://tomiwa-code.vercel.app/",
     link2: "https://github.com/tomiwa-code/my-portfolio",
+  },
+  {
+    id: 5,
+    name: "TYTN wears",
+    utilities:
+      "React + Vite, Strapi, Cloudniary, Stripe, Tailwind Css, Framer motion, Css",
+    video:
+      "https://res.cloudinary.com/dgdoymhtj/video/upload/v1679863985/my-portfolio/tytn_ixm6gg.mp4",
+    img: "https://res.cloudinary.com/dgdoymhtj/image/upload/v1679864004/my-portfolio/tytn_mvry8h.png",
+    link1: "https://tytn-wears.vercel.app/",
+    link2: "https://github.com/tomiwa-code/tytn_wears.git",
   },
 ];
